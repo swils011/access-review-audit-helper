0d6bf78c8f050ac1cecbd0cacad5d7c80002c2a8
Samuel Wilson <sammy.t.wilson@outlook.com>
2026-01-31 17:00:27 -0500

    Finalize sample data to match access review output schema

--- Untitled-1.ps1
+++
@@ -1,6 +0,0 @@
-pwsh .\scripts\export-access-review.ps1
-Invoke-Item .\output\access-review-results.csv
-
-
-git config --global user.name "Samuel Wilson"
-git config --global user.email "sammy.t.wilson@outlook."
