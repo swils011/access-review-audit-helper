463fc887891d08e2be1f3c992bebb5139abe99f4
Samuel Wilson <sammy.t.wilson@outlook.com>
2026-01-31 16:42:10 -0500

    Add access review export script and sample output

---
+++ Untitled-1.ps1
@@ -0,0 +1,6 @@
+pwsh .\scripts\export-access-review.ps1
+Invoke-Item .\output\access-review-results.csv
+
+
+git config --global user.name "Samuel Wilson"
+git config --global user.email "sammy.t.wilson@outlook."
